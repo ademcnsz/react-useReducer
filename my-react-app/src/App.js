fd4da18d5fcd31a3d523598792791a456f02b63b
Adem CANSIZ <ademmcansiz@gmail.com>
2023-01-05 02:10:48 +0300

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+
+  const [todo,setTodo] = useState([]);
+  const [todos,setTodos] = useState([]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setTodos([...todos, todo])
+    setTodo("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Todo List</h1>
+
+      <form onSubmit={handleSubmit}>
+          <input value={todo} onChange={(e)=> setTodo(e.target.value)}></input>
+          <button disabled={!todo} type='submit'>Ekle</button>
+      </form>
+      <ul>
+
+        {todos.map((a,i)=>
+        (<li key={i}>{a}</li>)
+        )}
+      </ul>
     </div>
   );
 }
