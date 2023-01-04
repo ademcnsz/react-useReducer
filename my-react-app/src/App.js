49a2a18c7329d8d381ae9d8ee3b9c3094ddca606
Adem CANSIZ <ademmcansiz@gmail.com>
2023-01-05 02:32:03 +0300

    Updates

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,29 +1,45 @@
 import './App.css';
-import {useState} from "react"
+import {useState,useReducer} from "react"
+import TodoReducer from "./TodoReducer"
+
 
 function App() {
 
-  const [todo,setTodo] = useState([]);
-  const [todos,setTodos] = useState([]);
+  const [state, dispatch] = useReducer(TodoReducer,{
+    todos:[],
+    todo : ""
+  });
+
+  // const [todo,setTodo] = useState([]);
+  // const [todos,setTodos] = useState("");
 
   const handleSubmit = (e)=>{
     e.preventDefault();
-    setTodos([...todos, todo])
-    setTodo("")
+    // setTodos([...todos, todo])
+    // setTodo("")
+    dispatch({
+      type:"ADD_TODO",
+      todo:state.todo
+    }) 
   }
-
+    const onChange = (e)=>{
+      dispatch({
+        type:"SET_TODO",
+        value:e.target.value
+      }) 
+    }
   return (
     <div className="App">
       
       <h1>Todo List</h1>
 
       <form onSubmit={handleSubmit}>
-          <input value={todo} onChange={(e)=> setTodo(e.target.value)}></input>
-          <button disabled={!todo} type='submit'>Ekle</button>
+          <input value={state.todo} onChange={onChange}></input>
+          <button disabled={!state.todo} type='submit'>Ekle</button>
       </form>
       <ul>
 
-        {todos.map((a,i)=>
+        {state.todos.map((a,i)=>
         (<li key={i}>{a}</li>)
         )}
       </ul>
